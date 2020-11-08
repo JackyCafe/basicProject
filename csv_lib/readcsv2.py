2d85656a3a56b1bb5c8ec0917b044f4f76d9151f
yhlin <powerworker1234@gmail.com>
2020-11-08 10:01:20 +0800

    1108 CSV

--- csv_lib/readcsv2.py
+++ csv_lib/readcsv2.py
@@ -1,8 +1,19 @@
 import pandas as pd
-file= "../data/A121151.csv"
-df = pd.read_csv(file)
+from pandas.core.generic import NDFrame
+import logging
+from toolkit.Judy import Math
+
+path = "../data/"
+logging.basicConfig(level=logging.INFO,filename='mylog.log',filemode='w')
+
+df = pd.read_csv(path+"A121151.csv")
 print(df)
 keys = df.keys()
 print(keys)
+series1: NDFrame
 series1 = df.get(keys[0])
-print(series1)
+series1 = series1[2:]
+y = series1.__array__(float) #to arr
+series1.to_csv(path+"A121153.csv",header=False,index=False)
+
+
